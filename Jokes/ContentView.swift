372eb0d6a74052574061ab9acf0d776e82d5a2d1
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-10 21:42:12 +0800

    - Project Creation - Implemented Joke struct - Code Complete

--- Jokes/ContentView.swift
+++ Jokes/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var jokeText = "Click Button"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text(jokeText)
+            Spacer()
+            Button {
+                getJoke()
+            } label: {
+                Text("Get Random Joke")
+            }
         }
         .padding()
+    }
+    
+    func getJoke() {
+        Task {
+            if let url = URL(string: "https://v2.jokeapi.dev/joke/Any?type=single") {
+                do {
+                    // Make the API call
+                    let (data, _) = try await URLSession.shared.data(from: url)
+                    
+                    // Parse the JSON data
+                    let decoder = JSONDecoder()
+                    let joke = try decoder.decode(Joke.self, from: data)
+                    
+                    // Assign joke text to label
+                    jokeText = joke.joke
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
---
+++ Jokes/Joke.swift
@@ -0,0 +1,12 @@
+//
+//  Joke.swift
+//  Jokes
+//
+//  Created by Kelvin on 6/10/24.
+//
+
+import Foundation
+
+struct Joke: Decodable {
+    var joke: String
+}
