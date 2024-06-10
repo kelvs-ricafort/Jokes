b4c6c63e0bb4d1418b7f17f7d0fc0c5d30ee4853
Kelvin Ricafort <kelvs.r@outlook.com>
2024-06-10 21:28:24 +0800

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Kelvin on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
